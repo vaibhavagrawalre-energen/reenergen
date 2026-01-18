018cd1f6609d80460284b9a1efac8ffa52931ec3
vaibhavagrawalre-energen <vaibhav.agrawal@re-energen.com>
2026-01-18 23:29:29 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -47,8 +47,8 @@ export default function HomePage() {
 
       <VendorsFinanceSection />
 
-      /*{/* Testimonials */}
-      <TestimonialsSection t={t} />*/
+      
+      {/*<TestimonialsSection t={t} />*/}
     </main>
   )
 }
